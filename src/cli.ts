873b8ac5e22e98bf6ab2076fc4bdaf62fe9d849d
webdevbynight <git@victor-brito.dev>
2025-02-03 11:08:36 +0100

    build(cli): parse CLI options and show version

---
+++ src/cli.ts
@@ -0,0 +1,119 @@
+import type { Args, CommandOption, ParsedOptions } from "./types.js";
+
+import process from "node:process";
+
+import packageManager from "../package.json" with { type: "json" };
+
+const availableOptions = {
+  branches: {
+    optionName: "branches",
+    flag: "--branches",
+    alias: "-b",
+    description: "Git branches to release from",
+    type: "array"
+  },
+  repositoryUrl: {
+    optionName: "repositoryUrl",
+    flag: "--repository-url",
+    alias: "-r",
+    description: "Git repository URL",
+    type: "string"
+  },
+  debug: {
+    optionName: "debug",
+    flag: "--debug",
+    description: "Output debugging information",
+    type: "boolean"
+  },
+  dryRun: {
+    optionName: "dryRun",
+    flag: "--dry-run",
+    alias: "-d",
+    description: "Skip release and publishing",
+    type: "boolean"
+  },
+  version: {
+    optionName: "version",
+    flag: "--version",
+    alias: "-v",
+    description: "Show version number",
+    type: "boolean"
+  },
+  help: {
+    optionName: "help",
+    flag: "--help",
+    alias: "-h",
+    description: "Show help",
+    type: "boolean"
+  }
+} as const;
+const allowedCommandOptions = Object.values(availableOptions).reduce(
+  (acc, option) => {
+    acc[option.flag] = option;
+    if ("alias" in option && option.alias) {
+      acc[option.alias] = option;
+    }
+    return acc;
+  },
+  {} as Record<CommandOption, (typeof availableOptions)[keyof typeof availableOptions]>
+);
+
+/**
+ * Parses the options run from the CLI.
+ * @param args - The arguments from the process.
+ * @return The options as defined by `availableOptions`.
+ */
+export const parseOptions = (args: Args): ParsedOptions => {
+  const parsedOptions: ParsedOptions = {};
+  let currentOption: keyof typeof availableOptions | null = null;
+  for (const arg of args) {
+    if (arg.startsWith("-")) {
+      if (!(arg in allowedCommandOptions)) continue;
+      const allowedCommandOption = allowedCommandOptions[arg as CommandOption];
+      if (allowedCommandOption) {
+        const { optionName, type } = allowedCommandOption;
+        currentOption = optionName;
+        switch (type) {
+          case "array":
+            Object.assign(parsedOptions, { [currentOption]: [] });
+            break;
+          case "string":
+            Object.assign(parsedOptions, { [currentOption]: "" });
+            break;
+          default:
+            Object.assign(parsedOptions, { [currentOption]: true });
+            break;
+        }
+      }
+    } else {
+      if (currentOption && currentOption in parsedOptions) {
+        const parsedOption = parsedOptions[currentOption];
+        if (typeof parsedOption !== "undefined") {
+          if (Array.isArray(parsedOption)) parsedOption.push(arg);
+          else if (typeof parsedOption === "string")
+            Object.assign(parsedOptions, { [currentOption]: arg });
+        }
+      }
+    }
+  }
+  return parsedOptions;
+};
+
+/**
+ * Shows the current version of `release-change`.
+ */
+export const showVersion = (): void => console.log(`v${packageManager.version}`);
+
+const cli = (): number => {
+  const args = process.argv.slice(2) as Args;
+  const parsedOptions = parseOptions(args);
+  const { help, version, ...options } = parsedOptions;
+  if (help) {
+    showVersion();
+    return 0;
+  }
+  console.log(help, version, options);
+  return 0;
+};
+
+export default cli;
---
+++ src/types.ts
@@ -0,0 +1,44 @@
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+] as const;
+type Letter = (typeof letters)[number];
+type Alias = `-${Lowercase<Letter> | Uppercase<Letter>}`;
+type Flag = `--${string}`;
+export type CommandOption = Alias | Flag;
+export type Args = (Alias | Flag | string)[];
+export type Help = true;
+export type Version = true;
+export type ParsedOptions = {
+  branches?: string[];
+  repositoryUrl?: string;
+  debug?: true;
+  dryRun?: true;
+  version?: true;
+  help?: true;
+};
+export type Options = Omit<ParsedOptions, "help" | "version">;
