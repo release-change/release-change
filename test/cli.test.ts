9405b2a7a7f1700ba0d985e4df585d73c8159066
webdevbynight <git@victor-brito.dev>
2025-02-06 16:57:33 +0100

    test: add test for `--help` CLI option

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -4,7 +4,7 @@ import childProcess from "node:child_process";
 
 import { assert, afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
-import { parseOptions, showVersion } from "../dist/cli.js";
+import { parseOptions, showHelp, showVersion } from "../dist/cli.js";
 
 import packageManager from "../package.json" with { type: "json" };
 
@@ -35,6 +35,38 @@ describe("options parsing", () => {
     const args: Args = ["--branches", "main", "--anOptionThatDoesNotExist"];
     const options = parseOptions(args);
     expect("anOptionThatDoesNotExist" in options).toBe(false);
+  });
+});
+
+describe("display help for `release-change` command", () => {
+  const expectedOutput = `Runs automated package release and publishing
+
+Usage:
+  release-change [options]
+
+Options
+  -b, --branches        Git branches to release from    [array]
+  -r, --repository-url  Git repository URL             [string]
+      --debug           Output debugging information  [boolean]
+  -d, --dry-run         Skip release and publishing   [boolean]
+  -v, --version         Show version number           [boolean]
+  -h, --help            Show help                     [boolean]`;
+  let originalConsoleLog: typeof console.log;
+
+  beforeEach(() => {
+    originalConsoleLog = console.log;
+    console.log = vi.fn();
+  });
+
+  afterEach(() => {
+    console.log = originalConsoleLog;
+    vi.restoreAllMocks();
+  });
+
+  it("should display the help content", () => {
+    vi.spyOn(childProcess, "execSync").mockReturnValue("npx release-change -h");
+    showHelp();
+    expect(console.log).toHaveBeenCalledWith(expectedOutput);
   });
 });
 
