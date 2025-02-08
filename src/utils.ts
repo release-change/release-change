281bc8926c8d092d2d9ad6eb0ca11cb35457ec03
webdevbynight <git@victor-brito.dev>
2025-02-08 09:40:20 +0100

    refactor(utils): move `displayOptions()` and group it with `showHelp()`

--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,6 @@
 import type { Args, CommandOption, ParsedOptions } from "./types.js";
 
-import { AVAILABLE_OPTIONS, TAB } from "./utils/constants.js";
-
-/**
- * Display the options when running the CLI with the `--help` or `-h` options.
- * @return The options, with their alias (if available), flag, description and type.
- */
-export const displayOptions = (): string => {
-  const availableOptionsValues = Object.values(AVAILABLE_OPTIONS);
-  const aliasMaxLength = Math.max(
-    ...availableOptionsValues.map(option =>
-      "alias" in option && option.alias ? option.alias.length : 0
-    )
-  );
-  const flagMaxLength = Math.max(...availableOptionsValues.map(option => option.flag.length));
-  const descriptionMaxLength = Math.max(
-    ...availableOptionsValues.map(option => option.description.length)
-  );
-  const typeMaxLength = Math.max(...availableOptionsValues.map(option => option.type.length)) + 2;
-  const header = "Options";
-  const options: string[] = [];
-  for (const option of availableOptionsValues) {
-    const alias = "alias" in option ? `${option.alias}, ` : " ".repeat(aliasMaxLength + 2);
-    const optionName = `${TAB}${alias}${option.flag.padEnd(flagMaxLength, " ")}`;
-    const description = `${option.description.padEnd(descriptionMaxLength, " ")}`;
-    const type = `[${option.type}]`.padStart(typeMaxLength, " ");
-    const optionElements = [optionName, description, type];
-    options.push(optionElements.join(TAB));
-  }
-  return `${header}\n${options.join("\n")}`;
-};
+import { AVAILABLE_OPTIONS } from "./utils/constants.js";
 
 /**
  * Parses the options run from the CLI.
--- src/utils/show-help.ts
+++ src/utils/show-help.ts
@@ -1,5 +1,33 @@
-import { displayOptions } from "../utils.js";
-import { TAB } from "./constants.js";
+import { AVAILABLE_OPTIONS, TAB } from "./constants.js";
+
+/**
+ * Display the options when running the CLI with the `--help` or `-h` options.
+ * @return The options, with their alias (if available), flag, description and type.
+ */
+const displayOptions = (): string => {
+  const availableOptionsValues = Object.values(AVAILABLE_OPTIONS);
+  const aliasMaxLength = Math.max(
+    ...availableOptionsValues.map(option =>
+      "alias" in option && option.alias ? option.alias.length : 0
+    )
+  );
+  const flagMaxLength = Math.max(...availableOptionsValues.map(option => option.flag.length));
+  const descriptionMaxLength = Math.max(
+    ...availableOptionsValues.map(option => option.description.length)
+  );
+  const typeMaxLength = Math.max(...availableOptionsValues.map(option => option.type.length)) + 2;
+  const header = "Options";
+  const options: string[] = [];
+  for (const option of availableOptionsValues) {
+    const alias = "alias" in option ? `${option.alias}, ` : " ".repeat(aliasMaxLength + 2);
+    const optionName = `${TAB}${alias}${option.flag.padEnd(flagMaxLength, " ")}`;
+    const description = `${option.description.padEnd(descriptionMaxLength, " ")}`;
+    const type = `[${option.type}]`.padStart(typeMaxLength, " ");
+    const optionElements = [optionName, description, type];
+    options.push(optionElements.join(TAB));
+  }
+  return `${header}\n${options.join("\n")}`;
+};
 
 /**
  * Shows the help for the `release-change` command.
