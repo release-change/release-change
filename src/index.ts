f783e2b0fb17f4a972fe3bcce8000c4610c2aa99
webdevbynight <git@victor-brito.dev>
2025-02-08 11:36:15 +0100

    refactor(check-requirements): get package name from `package.json`

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,7 @@ export const isGitVersionCompatible = (gitVersion: NonNullable<SemVer>) => {
  */
 const checkRequirements = (): void => {
   const { version } = process;
+  const packageName = packageManager.name;
   const requiredNodeVersions = packageManager.engines.node;
   if (!isNodeVersionCompatible(version, requiredNodeVersions)) {
     const formattedRequiredNodeVersions = requiredNodeVersions
@@ -42,14 +43,14 @@ const checkRequirements = (): void => {
       .replaceAll(" || ", " or ");
     const foundVersion = version.replace("v", "");
     console.error(
-      `[release-change]: Required one of the following Node versions: ${formattedRequiredNodeVersions}. Found ${foundVersion}.`
+      `[${packageName}]: Required one of the following Node versions: ${formattedRequiredNodeVersions}. Found ${foundVersion}.`
     );
     process.exit(1);
   }
   const gitVersion = semver.coerce(childProcess.execSync("git --version", { encoding: "utf8" }));
   if (!gitVersion || !isGitVersionCompatible(gitVersion)) {
     console.error(
-      `[release-change]: Git version ${GIT_MIN_VERSION} required. Found ${gitVersion}.`
+      `[${packageName}]: Git version ${GIT_MIN_VERSION} required. Found ${gitVersion}.`
     );
     process.exit(1);
   }
