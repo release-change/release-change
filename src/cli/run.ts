c8a544dcf0a772a3f698e03cf15506fd983d259b
webdevbynight <git@victor-brito.dev>
2025-02-14 01:46:44 +0100

    refactor(config): refactor export from main entry point

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -4,7 +4,7 @@ import childProcess from "node:child_process";
 import fs from "node:fs";
 import util from "node:util";
 
-import config from "../config/index.js";
+import getConfig from "../config/index.js";
 import setLogger from "../logger/index.js";
 
 import packageManager from "../../package.json" with { type: "json" };
@@ -12,11 +12,11 @@ import packageManager from "../../package.json" with { type: "json" };
 const run = (cliOptions: CliOptions, context: Context): void => {
   const { name: packageName, version: packageVersion } = packageManager;
   const { cwd } = context;
-  context.config = config(cliOptions);
+  context.config = getConfig(cliOptions);
   context.logger = setLogger(context.config.debug);
-  const { logger } = context;
+  const { config, logger } = context;
   logger.logInfo(`Running ${packageName} version ${packageVersion}`);
-  if (context.config.debug) {
+  if (config.debug) {
     logger.logInfo("Running in debug mode");
     logger.logDebug("Load context", "context");
     logger.logDebug(util.inspect(context, { depth: Number.POSITIVE_INFINITY }), "context");
@@ -24,10 +24,10 @@ const run = (cliOptions: CliOptions, context: Context): void => {
     const isConfigFile = fs.existsSync(`${cwd}/.releasechangerc`);
     if (isConfigFile) logger.logDebug(`Config loaded from \`${cwd}/.releasechangerc\``, "debug");
     else logger.logDebug("Config file not found", "config");
-    logger.logDebug(util.inspect(context.config, { depth: Number.POSITIVE_INFINITY }), "config");
+    logger.logDebug(util.inspect(config, { depth: Number.POSITIVE_INFINITY }), "config");
   }
-  const runAutomatedReleaseText = `Run automated release from branch ${childProcess.execSync("git branch --show-current", { encoding: "utf8" })} on repository ${context.config.repositoryUrl}`;
-  if (context.config.dryRun) logger.logWarn(`${runAutomatedReleaseText} in dry run mode`);
+  const runAutomatedReleaseText = `Run automated release from branch ${childProcess.execSync("git branch --show-current", { encoding: "utf8" })} on repository ${config.repositoryUrl}`;
+  if (config.dryRun) logger.logWarn(`${runAutomatedReleaseText} in dry run mode`);
   else logger.logSuccess(runAutomatedReleaseText);
 };
 
