9c9316c6e65caa9e98183442778ee9cab7862243
webdevbynight <git@victor-brito.dev>
2025-02-21 00:39:57 +0100

    test(config): add tests for `getConfigFile`

---
+++ test/config/get-config-file.test.ts
@@ -0,0 +1,21 @@
+import fs from "node:fs";
+
+import { describe, expect, it, vi } from "vitest";
+
+import { getConfigFile } from "../../src/config/get-config-file.js";
+
+describe("get config file", () => {
+  const mockedConfigFile = {
+    branches: ["main"]
+  };
+
+  it("should return `null` when no config file is found", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(false);
+    expect(getConfigFile()).toBe(null);
+  });
+  it("should return the config file content", () => {
+    vi.spyOn(fs, "existsSync").mockReturnValue(true);
+    vi.spyOn(fs, "readFileSync").mockReturnValue(JSON.stringify(mockedConfigFile));
+    expect(getConfigFile()).toEqual(mockedConfigFile);
+  });
+});
