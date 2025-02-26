a29dc308296c31f36563ba2631d0a3a4ee4aa346
webdevbynight <git@victor-brito.dev>
2025-02-26 02:01:49 +0100

    feat(cli): add step checking push permissions

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -3,6 +3,7 @@ import type { CliOptions, Context } from "./cli.types.js";
 import { debugConfig } from "../config/debug-config.js";
 import { getConfig } from "../config/index.js";
 import { checkBranch } from "../git/check-branch.js";
+import { checkPushPermissions } from "../git/check-push-permissions.js";
 import { checkRepository } from "../git/check-repository.js";
 import { setLogger } from "../logger/index.js";
 
@@ -18,5 +19,6 @@ export const run = async (cliOptions: CliOptions, context: Context): Promise<voi
   checkBranch(context);
   console.log("context.branch", context.branch);
   console.log("context.config.dryRun", context.config.dryRun);
+  await checkPushPermissions(context.config.repositoryUrl, context);
   console.log("exit", process.exitCode, process.exitCode ?? 0);
 };
