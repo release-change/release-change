bc13eeeb0c0c57212ef7f634ec41f610e29438a2
webdevbynight <git@victor-brito.dev>
2025-03-10 08:33:29 +0100

    refactor(release): refactor the code getting the latest valid Git tag

--- src/release/set-last-release.ts
+++ src/release/set-last-release.ts
@@ -3,70 +3,45 @@ import type { LastRelease } from "./release.types.js";
 
 import semver from "semver";
 
-import { getAllTags } from "../git/get-all-tags.js";
+import { getLatestValidTag } from "../git/get-latest-valid-tag.js";
 import { setLogger } from "../logger/index.js";
 import { getRootPackageVersion } from "./get-root-package-version.js";
 
+/**
+ * Sets the last release from the latest valid Git tag or the root package version and adds it to the context where the CLI is running.
+ * @param context - The context where the CLI is running.
+ */
 export const setLastRelease = (context: Context): void => {
   const { cwd, branch, config } = context;
   const logger = context.logger ?? setLogger();
   if (!branch || !config?.branches.includes(branch)) return;
-  const lastRelease: LastRelease = {
-    gitTag: null,
-    version: "0.0.0"
-  };
-  const gitTags = getAllTags(context);
-  if (gitTags.length) {
-    const branchReleaseType = config?.releaseType[branch];
-    if (branchReleaseType) {
-      const validGitTags = gitTags.filter(gitTag => {
-        const validTag = semver.valid(gitTag);
-        if (validTag) {
-          if (branchReleaseType.prerelease) {
-            const prereleaseIdentifiers = semver.prerelease(validTag);
-            if (prereleaseIdentifiers) {
-              const [prereleaseIdentifier] = prereleaseIdentifiers;
-              return prereleaseIdentifier === branchReleaseType.prereleaseIdentifier;
-            }
-            return false;
-          }
-          return !semver.prerelease(validTag);
-        }
-        return false;
-      });
-      const [gitTag] = validGitTags;
-      const version = semver.valid(gitTag);
-      if (gitTag && version) {
+  try {
+    const lastRelease: LastRelease = {
+      gitTag: null,
+      version: "0.0.0"
+    };
+    const latestValidGitTag = getLatestValidTag(context);
+    if (latestValidGitTag) {
+      const version = semver.valid(latestValidGitTag);
+      if (version) {
         logger.logInfo(
-          `Found Git tag ${gitTag} associated with version ${version} on branch ${branch}.`
+          `Found Git tag ${latestValidGitTag} associated with version ${version} on branch ${branch}.`
         );
-        lastRelease.gitTag = gitTag;
+        lastRelease.gitTag = latestValidGitTag;
         lastRelease.version = version;
-      } else {
-        logger.logError("Failed to get the last Git tag.");
-        process.exitCode = 1;
-      }
+      } else throw new Error("Failed to extract the version from the latest Git tag.");
     } else {
-      const [gitTag] = gitTags;
-      const version = semver.valid(gitTag);
-      if (gitTag && version) {
-        logger.logInfo(
-          `Found Git tag ${gitTag} associated with version ${version} on branch ${branch}.`
-        );
-        lastRelease.gitTag = gitTag;
-        lastRelease.version = version;
-      } else {
-        logger.logError("Failed to get the last Git tag.");
-        process.exitCode = 1;
-      }
+      logger.logInfo(`No Git tag version found on branch ${branch}.`);
+      const rootPackageVersion = semver.valid(getRootPackageVersion(cwd));
+      if (rootPackageVersion) {
+        logger.logInfo(`Found package version ${rootPackageVersion} on branch ${branch}.`);
+        lastRelease.version = rootPackageVersion;
+      } else logger.logInfo("No package version found.");
     }
-  } else {
-    logger.logInfo(`Not Git tag version found on branch ${branch}.`);
-    const rootPackageVersion = getRootPackageVersion(cwd);
-    if (rootPackageVersion) {
-      logger.logInfo(`Found package version ${rootPackageVersion} on branch ${branch}.`);
-      lastRelease.version = rootPackageVersion;
-    }
+    context.lastRelease = lastRelease;
+  } catch (error) {
+    if (error instanceof Error) logger.logError(error.message);
+    else logger.logError(`Unknown error: ${error}`);
+    process.exitCode = 1;
   }
-  context.lastRelease = lastRelease;
 };
