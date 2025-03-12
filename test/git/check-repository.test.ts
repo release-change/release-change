fcc1e9eab8f934ec5e45f779ce2c0f06fd928a3c
webdevbynight <git@victor-brito.dev>
2025-03-12 09:11:16 +0100

    refactor(git): exit with status code provided by Git command if repo checking does not succeed

--- test/git/check-repository.test.ts
+++ test/git/check-repository.test.ts
@@ -3,7 +3,7 @@ import process from "node:process";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { checkRepository } from "../../src/git/check-repository.js";
-import * as isGitRepositoryModule from "../../src/git/is-git-repository.js";
+import * as runCommandModule from "../../src/git/run-command.js";
 
 describe("check repository", () => {
   const mockedLogger = {
@@ -16,7 +16,7 @@ describe("check repository", () => {
   let originalProcessExit: typeof process.exit;
 
   beforeEach(() => {
-    vi.mock("../../src/git/is-git-repository.js");
+    vi.mock("../../src/git/run-command.js");
     originalProcessExit = process.exit;
     Object.defineProperty(process, "exit", {
       value: vi.fn(),
@@ -30,19 +30,37 @@ describe("check repository", () => {
     vi.restoreAllMocks();
   });
 
-  it("should call `process.exit(1)` if this is not a Git repository", async () => {
-    vi.mocked(isGitRepositoryModule.isGitRepository).mockReturnValue(Promise.resolve(false));
+  it("should call `process.exit(128)` if this is not a Git repository", async () => {
+    vi.mocked(runCommandModule.runCommand).mockReturnValue(
+      Promise.resolve({
+        status: 128,
+        stdout: "",
+        stderr: "Error"
+      })
+    );
     await checkRepository(mockedLogger);
-    expect(process.exit).toHaveBeenCalledWith(1);
+    expect(process.exit).toHaveBeenCalledWith(128);
   });
   it("should not call `process.exit()` if this is a Git repository", async () => {
-    vi.mocked(isGitRepositoryModule.isGitRepository).mockReturnValue(Promise.resolve(true));
+    vi.mocked(runCommandModule.runCommand).mockReturnValue(
+      Promise.resolve({
+        status: 0,
+        stdout: "",
+        stderr: ""
+      })
+    );
     await checkRepository(mockedLogger);
     expect(process.exit).not.toHaveBeenCalled();
   });
   it("should return 0 if this is a Git repository", async () => {
     vi.stubGlobal("process", { exitCode: 0 });
-    vi.mocked(isGitRepositoryModule.isGitRepository).mockReturnValue(Promise.resolve(true));
+    vi.mocked(runCommandModule.runCommand).mockReturnValue(
+      Promise.resolve({
+        status: 0,
+        stdout: "",
+        stderr: ""
+      })
+    );
     expect(await checkRepository(mockedLogger)).toBe(0);
     vi.unstubAllGlobals();
   });
