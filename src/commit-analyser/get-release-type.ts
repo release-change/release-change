88b11c09d9e2ed263eb9908aedcf5f391f31b5b9
webdevbynight <git@victor-brito.dev>
2025-03-14 08:36:25 +0100

    feat(commit-analyser): get release type from commits

---
+++ src/commit-analyser/get-release-type.ts
@@ -0,0 +1,57 @@
+import type { Context } from "../cli/cli.types.js";
+import type { ReleaseType } from "./commit-analyser.types.js";
+
+import { inspect } from "node:util";
+
+import { parseCommit } from "./parse-commit.js";
+import { setReleaseType } from "./set-release-type.js";
+
+/**
+ * Gets the release type from the commits committed since the previous release or the beginning.
+ * The release type can be one of the following, from most to least important:
+ * 1. `"major"`,
+ * 2. `"minor"`,
+ * 3. `"patch"`,
+ * 4. `null`.
+ * @param commits - The commits to analyse.
+ * @param context - The context where the CLI is running.
+ * @return The most important release type found if commits are parsed successfully, `null` if there are no commits or an error is thrown.
+ */
+export const getReleaseType = (commits: string[], context: Context): ReleaseType => {
+  const { config, logger } = context;
+  try {
+    const releaseTypes = new Set<ReleaseType>();
+    const totalCommits = commits.length;
+    if (totalCommits) {
+      for (const commit of commits) {
+        const parsedCommit = parseCommit(commit, context);
+        releaseTypes.add(setReleaseType(parsedCommit, context));
+      }
+      if (config.debug) logger.logDebug(inspect(releaseTypes), "commit-analyser:get-release-type");
+      const pluralRule = new Intl.PluralRules("en-GB", { type: "cardinal" });
+      const commitWord = pluralRule.select(totalCommits) === "one" ? "commit" : "commits";
+      const completeAnalysisSentence = `Analysis of ${totalCommits} ${commitWord} complete:`;
+      switch (true) {
+        case releaseTypes.has("major"):
+          logger.logSuccess(`${completeAnalysisSentence} major release.`);
+          return "major";
+        case releaseTypes.has("minor"):
+          logger.logSuccess(`${completeAnalysisSentence} minor release.`);
+          return "minor";
+        case releaseTypes.has("patch"):
+          logger.logSuccess(`${completeAnalysisSentence} patch release.`);
+          return "patch";
+        default:
+          logger.logSuccess(`${completeAnalysisSentence} no release.`);
+          return null;
+      }
+    }
+    logger.logWarn("No commits to analyse: no release.");
+    return null;
+  } catch (error) {
+    if (error instanceof Error) logger.logError(error.message);
+    else logger.logError(`Unknwon error: ${error}`);
+    process.exitCode = 1;
+    return null;
+  }
+};
