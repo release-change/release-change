727a22af11866938a599fb829408cb2f6cb42233
webdevbynight <git@victor-brito.dev>
2025-03-27 22:58:40 +0100

    style: make return type explicit

--- src/check-requirements/is-git-version-compatible.ts
+++ src/check-requirements/is-git-version-compatible.ts
@@ -9,6 +9,6 @@ import { GIT_MIN_VERSION } from "./constants.js";
  * @param gitVersion - The Git version installed.
  * @return `true` if the version installed matches the minimal version required, `false` otherwise.
  */
-export const isGitVersionCompatible = (gitVersion: NonNullable<SemVer>) => {
+export const isGitVersionCompatible = (gitVersion: NonNullable<SemVer>): boolean => {
   return gitVersion && semver.gte(gitVersion, GIT_MIN_VERSION);
 };
