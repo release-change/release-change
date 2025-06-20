eb90b97f1c822ff58ca75756e9376240ca002bff
webdevbynight <git@victor-brito.dev>
2025-06-20 23:48:38 +0200

    test(check-requirements): mock required Node versions

--- test/check-requirements/is-node-version-compatible.test.ts
+++ test/check-requirements/is-node-version-compatible.test.ts
@@ -2,25 +2,25 @@ import { describe, expect, it } from "vitest";
 
 import { isNodeVersionCompatible } from "../../src/check-requirements/is-node-version-compatible.js";
 
-import { REQUIRED_NODE_VERSIONS } from "../../src/check-requirements/constants.js";
+describe("Node version compatibility", () => {
+  const mockedRequiredNodeVersions = "^18.20.5 || ^20.18.3 || ^22.12.0";
 
-describe("incompatible Node version", () => {
   it("tests an outdated Node version", () => {
-    expect(isNodeVersionCompatible("v16.20.2", REQUIRED_NODE_VERSIONS)).toBe(false);
+    expect(isNodeVersionCompatible("v16.20.2", mockedRequiredNodeVersions)).toBe(false);
   });
   it("tests an odd Node version", () => {
-    expect(isNodeVersionCompatible("v23.7.0", REQUIRED_NODE_VERSIONS)).toBe(false);
+    expect(isNodeVersionCompatible("v23.7.0", mockedRequiredNodeVersions)).toBe(false);
   });
-});
-
-describe("compatible Node version", () => {
+  it("tests a future even Node version not supported yet", () => {
+    expect(isNodeVersionCompatible("v24.0.0", mockedRequiredNodeVersions)).toBe(false);
+  });
   it("tests a Node maintenance LTS version", () => {
-    expect(isNodeVersionCompatible("v18.20.6", REQUIRED_NODE_VERSIONS)).toBe(true);
+    expect(isNodeVersionCompatible("v18.20.6", mockedRequiredNodeVersions)).toBe(true);
   });
   it("tests a Node active LTS version", () => {
-    expect(isNodeVersionCompatible("v20.18.3", REQUIRED_NODE_VERSIONS)).toBe(true);
+    expect(isNodeVersionCompatible("v20.18.3", mockedRequiredNodeVersions)).toBe(true);
   });
   it("tests a Node current version (if even)", () => {
-    expect(isNodeVersionCompatible("v22.13.1", REQUIRED_NODE_VERSIONS)).toBe(true);
+    expect(isNodeVersionCompatible("v22.13.1", mockedRequiredNodeVersions)).toBe(true);
   });
 });
