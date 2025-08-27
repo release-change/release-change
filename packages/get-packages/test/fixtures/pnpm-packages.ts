db7a64627880e4ddb32b8f879f6b1338974020e4
webdevbynight <git@victor-brito.dev>
2025-08-28 00:45:43 +0200

    test(get-packages): add tests to `getPackages`

---
+++ packages/get-packages/test/fixtures/pnpm-packages.ts
@@ -0,0 +1,42 @@
+export const pnpmPackages = [
+  {
+    content: `packages:
+  - 'packages/*'
+
+catalog:
+  chalk: ^4.1.2
+
+catalogs:
+  react16:
+    react: ^16.7.0
+    react-dom: ^16.7.0
+  react17:
+    react: ^17.10.0
+    react-dom: ^17.10.0`,
+    patterns: { include: ["packages/*"], exclude: ["**/node_modules/**"] },
+    packages: [
+      { name: "@monorepo/a", path: "packages/a" },
+      { name: "@monorepo/b", path: "packages/b" }
+    ]
+  },
+  {
+    content: `packages:
+  # specify a package in a direct subdirectory of the root
+  - 'my-app'
+  # all packages in direct subdirectories of packages/
+  - 'packages/*'
+  # all packages in subdirectories of components/
+  - 'components/**'
+  # exclude packages that are inside test directories
+  - '!**/test/**'`,
+    patterns: {
+      include: ["my-app", "packages/*", "components/**"],
+      exclude: ["**/node_modules/**", "**/test/**"]
+    },
+    packages: [
+      { name: "@monorepo/my-app", path: "my-app" },
+      { name: "@monorepo/a", path: "packages/a" },
+      { name: "@monorepo/b", path: "components/b" }
+    ]
+  }
+];
---
+++ packages/get-packages/test/get-packages.test.ts
@@ -0,0 +1,111 @@
+import { afterEach, assert, beforeEach, describe, expect, it, vi } from "vitest";
+
+import { getPackageManager } from "../src/get-package-manager.js";
+import { getPackagesFromGlobPatterns } from "../src/get-packages-from-glob-patterns.js";
+import {
+  getNpmGlobPatterns,
+  getPackages,
+  getPnpmGlobPatterns,
+  getRootPackageManifest,
+  getRootPnpmWorkspaceManifest
+} from "../src/index.js";
+import { mockedCwd } from "./fixtures/mocked-cwd.js";
+import { npmPackages } from "./fixtures/npm-packages.js";
+import { pnpmPackages } from "./fixtures/pnpm-packages.js";
+
+const mockedContextBase = {
+  cwd: mockedCwd,
+  env: {},
+  config: { debug: false }
+};
+const expectedSinglePackage = [{ name: "", path: "." }];
+
+beforeEach(() => {
+  vi.mock("../src/get-package-manager.js", () => ({ getPackageManager: vi.fn() }));
+  vi.mock("../src/get-root-package-manifest.js", () => ({ getRootPackageManifest: vi.fn() }));
+  vi.mock("../src/get-root-pnpm-workspace-manifest.js", () => ({
+    getRootPnpmWorkspaceManifest: vi.fn()
+  }));
+  vi.mock("../src/get-npm-glob-patterns.js", () => ({ getNpmGlobPatterns: vi.fn() }));
+  vi.mock("../src/get-pnpm-glob-patterns.js", () => ({ getPnpmGlobPatterns: vi.fn() }));
+  vi.mock("../src/get-packages-from-glob-patterns.js", () => ({
+    getPackagesFromGlobPatterns: vi.fn()
+  }));
+});
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+it("should throw an error if the package manager is not found or supported", async () => {
+  vi.mocked(getPackageManager).mockReturnValue(null);
+  await expect(getPackages(mockedContextBase)).rejects.toThrow(
+    "Failed to get the package manager: it must be either `npm` or `pnpm`."
+  );
+});
+it("should throw an error if the package manager is npm and no `package.json` file is found at the root", async () => {
+  vi.mocked(getPackageManager).mockReturnValue("npm");
+  vi.mocked(getRootPackageManifest).mockImplementation(() => {
+    throw new Error("Failed to get the root package manifest (`package.json`): file not found.");
+  });
+  assert.throw(() => getRootPackageManifest(`${mockedCwd}/package.json`));
+  await expect(getPackages(mockedContextBase)).rejects.toThrow();
+});
+it("should throw an error if the package manager is pnpm and the glob patterns do not return anything", async () => {
+  vi.mocked(getPackageManager).mockReturnValue("pnpm");
+  vi.mocked(getRootPnpmWorkspaceManifest).mockReturnValue("no-packages:");
+  vi.mocked(getPnpmGlobPatterns).mockImplementation(() => {
+    throw new Error(
+      "Failed to get the glob patterns: the root `pnpm-workspace.yaml` file must have a `packages` field."
+    );
+  });
+  await expect(getPackages(mockedContextBase)).rejects.toThrow();
+});
+it("should return one single package when the package manager is npm and the glob patterns do not return anything", async () => {
+  vi.mocked(getPackageManager).mockReturnValue("npm");
+  vi.mocked(getRootPackageManifest).mockReturnValue({});
+  vi.mocked(getNpmGlobPatterns).mockReturnValue(null);
+  assert.deepEqual(await getPackages(mockedContextBase), expectedSinglePackage);
+});
+describe.each(npmPackages)("when the package manager is npm", ({ content, patterns, packages }) => {
+  it("should return one single package when the glob patterns return an empty array of packages", async () => {
+    vi.mocked(getPackageManager).mockReturnValue("npm");
+    vi.mocked(getRootPackageManifest).mockReturnValue(content);
+    vi.mocked(getNpmGlobPatterns).mockReturnValue(patterns);
+    vi.mocked(getPackagesFromGlobPatterns).mockResolvedValue([]);
+    assert.deepEqual(await getPackages(mockedContextBase), expectedSinglePackage);
+  });
+  it("should return the correct packages when the glob patterns return packages", async () => {
+    vi.mocked(getPackageManager).mockReturnValue("npm");
+    vi.mocked(getRootPackageManifest).mockReturnValue(content);
+    vi.mocked(getNpmGlobPatterns).mockReturnValue(patterns);
+    vi.mocked(getPackagesFromGlobPatterns).mockResolvedValue(packages);
+    assert.deepEqual(await getPackages(mockedContextBase), [...expectedSinglePackage, ...packages]);
+  });
+});
+it("should return one single package when the package manager is pnpm and no `pnpm-workspace.yaml` file is found at the root", async () => {
+  vi.mocked(getPackageManager).mockReturnValue("pnpm");
+  vi.mocked(getRootPnpmWorkspaceManifest).mockReturnValue(null);
+  assert.deepEqual(await getPackages(mockedContextBase), expectedSinglePackage);
+});
+describe.each(pnpmPackages)(
+  "when the package manager is pnpm",
+  ({ content, patterns, packages }) => {
+    it("should return one single package when the glob patterns return an empty array of packages", async () => {
+      vi.mocked(getPackageManager).mockReturnValue("npm");
+      vi.mocked(getRootPnpmWorkspaceManifest).mockReturnValue(content);
+      vi.mocked(getPnpmGlobPatterns).mockReturnValue(patterns);
+      vi.mocked(getPackagesFromGlobPatterns).mockResolvedValue([]);
+      assert.deepEqual(await getPackages(mockedContextBase), expectedSinglePackage);
+    });
+    it("should return the correct packages when the glob patterns return packages", async () => {
+      vi.mocked(getPackageManager).mockReturnValue("npm");
+      vi.mocked(getRootPnpmWorkspaceManifest).mockReturnValue(content);
+      vi.mocked(getPnpmGlobPatterns).mockReturnValue(patterns);
+      vi.mocked(getPackagesFromGlobPatterns).mockResolvedValue(packages);
+      assert.deepEqual(await getPackages(mockedContextBase), [
+        ...expectedSinglePackage,
+        ...packages
+      ]);
+    });
+  }
+);
