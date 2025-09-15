89c2845a92d570686b7a52f2e7f03f509854fca4
webdevbynight <git@victor-brito.dev>
2025-09-15 16:41:48 +0200

    test(git): prepare push

---
+++ packages/git/test/push.test.ts
@@ -0,0 +1,42 @@
+/** biome-ignore-all lint/correctness/noUnusedVariables: <TODO: drop this line when the command is run> */
+import { runCommand } from "@release-change/shared";
+import { afterEach, beforeEach, expect, it, vi } from "vitest";
+
+import { push } from "../src/push.js";
+import { mockedContext, mockedContextWithUndefinedBranchName } from "./fixtures/mocked-context.js";
+
+beforeEach(() => {
+  vi.mock("@release-change/shared", () => ({
+    runCommand: vi.fn()
+  }));
+});
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+it("should throw an error if the branch name is not defined", async () => {
+  await expect(push(mockedContextWithUndefinedBranchName)).rejects.toThrowError(
+    "A branch name must be provided."
+  );
+});
+it("should run `git push` command when the branch name is defined", async () => {
+  const mockedCommand = vi
+    .mocked(runCommand)
+    .mockResolvedValue({ status: 0, stdout: "", stderr: "" });
+  await push(mockedContext);
+  // TODO: uncomment when the command is run
+  // expect(mockedCommand).toHaveBeenCalledWith("git", ["push", "origin", "branch-name"]);
+});
+it("should run `git push` command with the `--follow-tags` option when tags must be included", async () => {
+  const mockedCommand = vi
+    .mocked(runCommand)
+    .mockResolvedValue({ status: 0, stdout: "", stderr: "" });
+  await push(mockedContext, { includeTags: true });
+  // TODO: uncomment when the command is run
+  // expect(mockedCommand).toHaveBeenCalledWith("git", [
+  //   "push",
+  //   "--follow-tags",
+  //   "origin",
+  //   "branch-name"
+  // ]);
+});
