2884d45c3a7444d6f9f527c89c446bea4ffc5c55
webdevbynight <git@victor-brito.dev>
2025-09-22 15:45:24 +0200

    feat(npm): prepare package publishing and publish only after push step

--- packages/npm/src/index.ts
+++ packages/npm/src/index.ts
@@ -1,1 +1,4 @@
+export type { PackagePublishing } from "./npm.types.js";
+
+export { preparePublishing } from "./prepare-publishing.js";
 export { publishToRegistry } from "./publish-to-registry.js";
