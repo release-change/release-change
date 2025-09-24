917d69400325a10f491864ac798665d20965a7d3
webdevbynight <git@victor-brito.dev>
2025-09-24 17:56:19 +0200

    test(ci): refactor test cases and group “static” tests into one single “dynamic-titled” test

--- packages/ci/test/configure-ci-environment.test.ts
+++ packages/ci/test/configure-ci-environment.test.ts
@@ -1,24 +1,8 @@
-import { afterEach, assert, beforeEach, it, vi } from "vitest";
+import { afterEach, assert, beforeEach, describe, it, vi } from "vitest";
 
 import { configureCiEnvironment } from "../src/index.js";
 import { isCiToolDetected } from "../src/is-ci-tool-detected.js";
-
-const mockedEnvWithUnknownCiEnvironment = {};
-const mockedEnvWithGitHubActionsOnPushEvent = {
-  CI: "true",
-  GITHUB_ACTIONS: "true",
-  GITHUB_EVENT_NAME: "push"
-};
-const mockedEnvWithGitHubActionsOnPullRequestEvent = {
-  CI: "true",
-  GITHUB_ACTIONS: "true",
-  GITHUB_EVENT_NAME: "pull_request"
-};
-const mockedEnvWithGitHubActionsOnPullRequestTargetEvent = {
-  CI: "true",
-  GITHUB_ACTIONS: "true",
-  GITHUB_EVENT_NAME: "pull_request_target"
-};
+import { mockedEnvs } from "./fixtures/mocked-envs.js";
 
 beforeEach(() => {
   vi.mock("../src/is-ci-tool-detected.js", () => ({
@@ -29,41 +13,10 @@ afterEach(() => {
   vi.clearAllMocks();
 });
 
-it("should return an object with `isCi` set to `false` and `isPullRequest` set to `false` when CI environment is unknown", () => {
-  const expectedConfig = {
-    isCi: false,
-    isPullRequest: false
-  };
-  vi.mocked(isCiToolDetected).mockReturnValue(false);
-  assert.deepEqual(configureCiEnvironment(mockedEnvWithUnknownCiEnvironment), expectedConfig);
-});
-it("should return an object with `isCi` set to `true` and `isPullRequest` set to `false` when CI environment is GitHub Actions on push event", () => {
-  const expectedConfig = {
-    isCi: true,
-    isPullRequest: false
-  };
-  vi.mocked(isCiToolDetected).mockReturnValue(true);
-  assert.deepEqual(configureCiEnvironment(mockedEnvWithGitHubActionsOnPushEvent), expectedConfig);
-});
-it("should return an object with `isCi` set to `true` and `isPullRequest` set to `true` when CI environment is GitHub Actions on pull_request event", () => {
-  const expectedConfig = {
-    isCi: true,
-    isPullRequest: true
-  };
-  vi.mocked(isCiToolDetected).mockReturnValue(true);
-  assert.deepEqual(
-    configureCiEnvironment(mockedEnvWithGitHubActionsOnPullRequestEvent),
-    expectedConfig
-  );
-});
-it("should return an object with `isCi` set to `true` and `isPullRequest` set to `true` when CI environment is GitHub Actions on pull_request_target event", () => {
-  const expectedConfig = {
-    isCi: true,
-    isPullRequest: true
-  };
-  vi.mocked(isCiToolDetected).mockReturnValue(true);
-  assert.deepEqual(
-    configureCiEnvironment(mockedEnvWithGitHubActionsOnPullRequestTargetEvent),
-    expectedConfig
-  );
+describe.each(mockedEnvs)("for $env", ({ ciEnvironment, env, expectedConfig }) => {
+  const { isCi, isPullRequest } = expectedConfig;
+  it(`should return an object with \`isCi\` set to \`${isCi}\` and \`isPullRequest\` set to \`${isPullRequest}\` when CI environment is ${ciEnvironment}`, () => {
+    vi.mocked(isCiToolDetected).mockReturnValue(isCi);
+    assert.deepEqual(configureCiEnvironment(env), expectedConfig);
+  });
 });
