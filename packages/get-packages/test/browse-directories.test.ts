1d7c75a252be40aa69adf003b6c6f217c3c31c9c
webdevbynight <git@victor-brito.dev>
2025-10-21 09:06:59 +0200

    test(get-packages): fix `Dirent` typing
    The update of @types/node to 22.18.12 made the tests fail.

--- packages/get-packages/test/browse-directories.test.ts
+++ packages/get-packages/test/browse-directories.test.ts
@@ -33,49 +33,51 @@ it("should return an empty array if the directory is empty", async () => {
   expect(await browseDirectories(mockedCwd)).toStrictEqual([]);
 });
 it("should browse directories recursively", async () => {
-  vi.spyOn(fs, "readdir").mockImplementation(async dirPath => {
-    if (dirPath === mockedCwd) {
-      return [
-        {
-          ...mockedDirent,
-          name: "directory1" as unknown as Buffer<ArrayBufferLike>,
-          isDirectory: () => true,
-          parentPath: mockedCwd,
-          path: "/fake/cwd/directory1"
-        },
-        {
-          ...mockedDirent,
-          name: "directory2" as unknown as Buffer<ArrayBufferLike>,
-          isDirectory: () => true,
-          parentPath: mockedCwd,
-          path: "/fake/cwd/directory2"
-        }
-      ];
+  vi.spyOn(fs, "readdir").mockImplementation(
+    async (dirPath): Promise<Dirent<Buffer<ArrayBuffer>>[]> => {
+      if (dirPath === mockedCwd) {
+        return [
+          {
+            ...mockedDirent,
+            name: "directory1" as unknown as Buffer<ArrayBuffer>,
+            isDirectory: () => true,
+            parentPath: mockedCwd,
+            path: "/fake/cwd/directory1"
+          },
+          {
+            ...mockedDirent,
+            name: "directory2" as unknown as Buffer<ArrayBuffer>,
+            isDirectory: () => true,
+            parentPath: mockedCwd,
+            path: "/fake/cwd/directory2"
+          }
+        ];
+      }
+      if (dirPath === "/fake/cwd/directory1") {
+        return [
+          {
+            ...mockedDirent,
+            name: "subdirectory1" as unknown as Buffer<ArrayBuffer>,
+            isDirectory: () => true,
+            parentPath: "/fake/cwd/directory1",
+            path: "/fake/cwd/directory1/subdirectory1"
+          }
+        ];
+      }
+      if (dirPath === "/fake/cwd/directory2") {
+        return [
+          {
+            ...mockedDirent,
+            name: "subdirectory2" as unknown as Buffer<ArrayBuffer>,
+            isDirectory: () => true,
+            parentPath: "/fake/cwd/directory2",
+            path: "/fake/cwd/directory2/subdirectory2"
+          }
+        ];
+      }
+      return [];
     }
-    if (dirPath === "/fake/cwd/directory1") {
-      return [
-        {
-          ...mockedDirent,
-          name: "subdirectory1" as unknown as Buffer<ArrayBufferLike>,
-          isDirectory: () => true,
-          parentPath: "/fake/cwd/directory1",
-          path: "/fake/cwd/directory1/subdirectory1"
-        }
-      ];
-    }
-    if (dirPath === "/fake/cwd/directory2") {
-      return [
-        {
-          ...mockedDirent,
-          name: "subdirectory2" as unknown as Buffer<ArrayBufferLike>,
-          isDirectory: () => true,
-          parentPath: "/fake/cwd/directory2",
-          path: "/fake/cwd/directory2/subdirectory2"
-        }
-      ];
-    }
-    return [];
-  });
+  );
   expect(await browseDirectories(mockedCwd)).toStrictEqual([
     "/fake/cwd/directory1",
     "/fake/cwd/directory1/subdirectory1",
