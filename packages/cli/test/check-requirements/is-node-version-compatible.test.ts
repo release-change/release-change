03646303fb067179069ca78ca08ca29390dd8563
webdevbynight <git@victor-brito.dev>
2025-10-29 12:49:43 +0100

    chore(node): add support for Node 24
    Since release-change has not been released yet, this commit does not introduce a new feature.

--- packages/cli/test/check-requirements/is-node-version-compatible.test.ts
+++ packages/cli/test/check-requirements/is-node-version-compatible.test.ts
@@ -2,7 +2,7 @@ import { expect, it } from "vitest";
 
 import { isNodeVersionCompatible } from "../../src/check-requirements/is-node-version-compatible.js";
 
-const mockedRequiredNodeVersions = "^18.20.5 || ^20.18.3 || ^22.12.0";
+const mockedRequiredNodeVersions = "^20.18.3 || ^22.12.0 || ^24.0.0";
 
 it("tests an outdated Node version", () => {
   expect(isNodeVersionCompatible("16.20.2", mockedRequiredNodeVersions)).toBe(false);
@@ -11,14 +11,14 @@ it("tests an odd Node version", () => {
   expect(isNodeVersionCompatible("23.7.0", mockedRequiredNodeVersions)).toBe(false);
 });
 it("tests a future even Node version not supported yet", () => {
-  expect(isNodeVersionCompatible("24.0.0", mockedRequiredNodeVersions)).toBe(false);
+  expect(isNodeVersionCompatible("42.0.0", mockedRequiredNodeVersions)).toBe(false);
 });
 it("tests a Node maintenance LTS version", () => {
-  expect(isNodeVersionCompatible("18.20.6", mockedRequiredNodeVersions)).toBe(true);
+  expect(isNodeVersionCompatible("20.18.4", mockedRequiredNodeVersions)).toBe(true);
 });
 it("tests a Node active LTS version", () => {
-  expect(isNodeVersionCompatible("20.18.3", mockedRequiredNodeVersions)).toBe(true);
+  expect(isNodeVersionCompatible("22.13.1", mockedRequiredNodeVersions)).toBe(true);
 });
 it("tests a Node current version (if even)", () => {
-  expect(isNodeVersionCompatible("22.13.1", mockedRequiredNodeVersions)).toBe(true);
+  expect(isNodeVersionCompatible("24.11.0", mockedRequiredNodeVersions)).toBe(true);
 });
