39eefd39a6a8875cfbc67fcfd86f0f6e5c6bb52a
webdevbynight <git@victor-brito.dev>
2025-10-29 21:15:03 +0100

    style(github): comment parts of code needing to run APIs for real

--- packages/github/src/post-fail-comment.ts
+++ packages/github/src/post-fail-comment.ts
@@ -56,50 +56,50 @@ ${errorsList.length ? errorsList.join("\n\n---\n\n") : "No errors reported."}
       body: commentBody
     };
     // TODO: uncomment to use GiHub API
-    const failCommentResponse = await fetch(uri, {
-      method: "POST",
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${issuePullRequestToken}`,
-        "Content-Type": "application/json",
-        "X-GitHub-Api-Version": "2022-11-28"
-      },
-      body: JSON.stringify(requestBody)
-    });
-    const { headers, status, statusText } = failCommentResponse;
-    const issueType = isPullRequest ? "pull request" : "issue";
-    const failCommentResponseData = failCommentResponse.json();
+    // const failCommentResponse = await fetch(uri, {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/vnd.github+json",
+    //     Authorization: `Bearer ${issuePullRequestToken}`,
+    //     "Content-Type": "application/json",
+    //     "X-GitHub-Api-Version": "2022-11-28"
+    //   },
+    //   body: JSON.stringify(requestBody)
+    // });
+    // const { headers, status, statusText } = failCommentResponse;
+    // const issueType = isPullRequest ? "pull request" : "issue";
+    // const failCommentResponseData = failCommentResponse.json();
     if (debug) {
       logger.setDebugScope("github:post-fail-comment");
       logger.logDebug(`API entry point: ${uri}`);
       logger.logDebug(`Request body: ${inspect(requestBody, { depth: Number.POSITIVE_INFINITY })}`);
-      logger.logDebug(`Response status: ${status}`);
-      logger.logDebug(`Response status text: ${statusText}`);
-      logger.logDebug(`Response headers: ${inspect(headers, { depth: Number.POSITIVE_INFINITY })}`);
-      logger.logDebug(
-        `Response JSON: ${inspect(await failCommentResponseData, { depth: Number.POSITIVE_INFINITY })}`
-      );
+      // logger.logDebug(`Response status: ${status}`);
+      // logger.logDebug(`Response status text: ${statusText}`);
+      // logger.logDebug(`Response headers: ${inspect(headers, { depth: Number.POSITIVE_INFINITY })}`);
+      // logger.logDebug(
+      //   `Response JSON: ${inspect(await failCommentResponseData, { depth: Number.POSITIVE_INFINITY })}`
+      // );
     }
     // TODO: uncomment when the API is used
-    if (status === 201) logger.logInfo(`Added fail comment on ${issueType} #${number}.`);
-    else if (status === 404)
-      logger.logWarn(
-        `The resource requested for ${issueType} #${number} has not been found; therefore, the fail comment has not been added.`
-      );
-    else {
-      const responseError: GitHubResponseError = await failCommentResponseData;
-      const { message, documentation_url: documentationUrl } = responseError;
-      const documentationReference = documentationUrl ? ` See ${documentationUrl}.` : "";
-      logger.logError(`Failed to post the fail comment on ${issueType} #${number}.`);
-      process.exitCode = status;
-      throw formatDetailedError({
-        title: "Failed to post the fail comment",
-        message: `${message}${documentationReference}`,
-        details: {
-          output: `status: ${status}`
-        }
-      });
-    }
+    // if (status === 201) logger.logInfo(`Added fail comment on ${issueType} #${number}.`);
+    // else if (status === 404)
+    //   logger.logWarn(
+    //     `The resource requested for ${issueType} #${number} has not been found; therefore, the fail comment has not been added.`
+    //   );
+    // else {
+    //   const responseError: GitHubResponseError = await failCommentResponseData;
+    //   const { message, documentation_url: documentationUrl } = responseError;
+    //   const documentationReference = documentationUrl ? ` See ${documentationUrl}.` : "";
+    //   logger.logError(`Failed to post the fail comment on ${issueType} #${number}.`);
+    //   process.exitCode = status;
+    //   throw formatDetailedError({
+    //     title: "Failed to post the fail comment",
+    //     message: `${message}${documentationReference}`,
+    //     details: {
+    //       output: `status: ${status}`
+    //     }
+    //   });
+    // }
   } else {
     process.exitCode = 1;
     throw formatDetailedError({
