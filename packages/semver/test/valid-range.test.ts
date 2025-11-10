92bfefa8e21f93a0adfe894146c035d099d1ffa8
webdevbynight <git@victor-brito.dev>
2025-11-10 16:00:22 +0100

    test(semver): add functions to validate a range

---
+++ packages/semver/test/valid-range.test.ts
@@ -0,0 +1,30 @@
+import { assert, expect, it } from "vitest";
+
+import { validRange } from "../src/index.js";
+import { invalidRanges } from "./fixtures/invalid-ranges.js";
+import { validRanges } from "./fixtures/valid-ranges.js";
+import { validRangesInLooseMode } from "./fixtures/valid-ranges-in-loose-mode.js";
+
+it.each(invalidRanges)("should return `null` if $raw is to be validated", ({ raw, options }) => {
+  expect(validRange(raw, options)).toBe(null);
+});
+it.each(validRangesInLooseMode)(
+  "should return `null` if $raw is to be validated in strict mode",
+  ({ raw }) => {
+    expect(validRange(raw)).toBe(null);
+  }
+);
+it.each(validRangesInLooseMode)(
+  "should return $expected.range if $raw is to be validated in loose mode",
+  ({ raw, expected }) => {
+    const { range } = expected;
+    assert.strictEqual(validRange(raw, { loose: true }), range);
+  }
+);
+it.each(validRanges)(
+  "should return $expected.range if $raw is to be validated",
+  ({ raw, expected, options }) => {
+    const { range } = expected;
+    assert.strictEqual(validRange(raw, options), range);
+  }
+);
