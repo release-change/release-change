5726d62274193aa3bdc544c3d3c2f00e339e00d9
webdevbynight <git@victor-brito.dev>
2026-01-15 12:25:15 +0100

    test(npm): enable publishing package to NPM

--- packages/npm/test/fixtures/mocked-package-managers.ts
+++ packages/npm/test/fixtures/mocked-package-managers.ts
@@ -1,15 +1,14 @@
 import type { PackageManager } from "@release-change/get-packages";
 
-// TODO: remove `--dry-run` flag when releases are truly published to the NPM registry
 export const packageManagers: {
   packageManager: NonNullable<PackageManager>;
   args: string[];
   noGitChecks?: string;
 }[] = [
-  { packageManager: "npm", args: ["publish", "--dry-run", "--access", "public"] },
+  { packageManager: "npm", args: ["publish", "--access", "public"] },
   {
     packageManager: "pnpm",
-    args: ["publish", "--dry-run", "--access", "public"],
+    args: ["publish", "--access", "public"],
     noGitChecks: "--no-git-checks"
   }
 ];
