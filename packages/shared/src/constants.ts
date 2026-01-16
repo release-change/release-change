cb757e88474f68d19167de7bcb6a7027c5f72020
webdevbynight <git@victor-brito.dev>
2026-01-16 06:51:05 +0100

    fix(shared): fix value of workspace name

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,5 +1,5 @@
 import rootPackageManifest from "../../../package.json" with { type: "json" };
 
 export const ROOT_PACKAGE_MANIFEST = rootPackageManifest;
-export const WORKSPACE_NAME = ROOT_PACKAGE_MANIFEST.name;
+export const WORKSPACE_NAME = "release-change";
 export const WORKSPACE_VERSION = ROOT_PACKAGE_MANIFEST.version;
